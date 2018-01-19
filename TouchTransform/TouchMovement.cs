f63f1be738992b32c65874e3e270f94db09a5dc5
Daniel Pots <daniel@vr-bits.de>
2018-01-19 18:43:08 +0100

    Initial Commit

---
+++ TouchTransform/TouchMovement.cs
@@ -0,0 +1,119 @@
+﻿//THX to Caue Rego (cawas), who wrote the original: http://wiki.unity3d.com/index.php/DetectTouchMovement
+
+using UnityEngine;
+
+public class TouchMovement : MonoBehaviour {
+    //Added rudimentry Singleton Pattern, feel free to change
+    public static TouchMovement instance;
+
+    void Awake() {
+        if (!instance) {
+            instance = this;
+            return;
+        }
+        Destroy(this);
+    }
+
+    public float pinchTurnRatio = Mathf.PI / 2;
+    public float minTurnAngle = 5;
+
+    public float pinchRatio = 1;
+    public float minPinchDistance = 3;
+
+    public float panRatio = 1;
+    public float minPanDistance = 3;
+
+    /// <summary>
+    ///   The delta of the angle between two touch points
+    /// </summary>
+    [HideInInspector]
+    public float turnAngleDelta;
+    /// <summary>
+    ///   The angle between two touch points
+    /// </summary>
+    [HideInInspector]
+    public float turnAngle;
+
+    /// <summary>
+    ///   The delta of the distance between two touch points that were distancing from each other
+    /// </summary>
+    [HideInInspector]
+    public float pinchDistanceDelta;
+    /// <summary>
+    ///   The distance between two touch points that were distancing from each other
+    /// </summary>
+    [HideInInspector]
+    public float pinchDistance;
+    [HideInInspector]
+    public Vector2 panDistance;
+
+    /// <summary>
+    ///   Calculates Pinch and Turn - This should be used inside LateUpdate
+    /// </summary>
+    void Update() {
+        pinchDistance = pinchDistanceDelta = 0;
+        turnAngle = turnAngleDelta = 0;
+
+        if (Input.touchCount == 1) {
+            var touch = Input.touches[0];
+            // ... if at least one of them moved ...
+            if (touch.phase == TouchPhase.Moved) {
+                if (Mathf.Abs(touch.deltaPosition.magnitude) > minPanDistance) {
+                    panDistance = touch.deltaPosition * panRatio;
+                } else {
+                    panDistance = Vector2.zero;
+                }
+            }
+        }
+
+        // if two fingers are touching the screen at the same time ...
+        if (Input.touchCount == 2) {
+            Touch touch1 = Input.touches[0];
+            Touch touch2 = Input.touches[1];
+
+            // ... if at least one of them moved ...
+            if (touch1.phase == TouchPhase.Moved || touch2.phase == TouchPhase.Moved) {
+                // ... check the delta distance between them ...
+                pinchDistance = Vector2.Distance(touch1.position, touch2.position);
+                float prevDistance = Vector2.Distance(touch1.position - touch1.deltaPosition,
+                                                      touch2.position - touch2.deltaPosition);
+                pinchDistanceDelta = pinchDistance - prevDistance;
+
+                // ... if it's greater than a minimum threshold, it's a pinch!
+                if (Mathf.Abs(pinchDistanceDelta) > minPinchDistance) {
+                    pinchDistanceDelta *= pinchRatio;
+                } else {
+                    pinchDistance = pinchDistanceDelta = 0;
+                }
+                Debug.Log("Pinchdistance: " + pinchDistance);
+
+                // ... or check the delta angle between them ...
+                turnAngle = Angle(touch1.position, touch2.position);
+                float prevTurn = Angle(touch1.position - touch1.deltaPosition,
+                                       touch2.position - touch2.deltaPosition);
+                turnAngleDelta = Mathf.DeltaAngle(prevTurn, turnAngle);
+
+                // ... if it's greater than a minimum threshold, it's a turn!
+                if (Mathf.Abs(turnAngleDelta) > minTurnAngle) {
+                    turnAngleDelta *= pinchTurnRatio;
+                } else {
+                    turnAngle = turnAngleDelta = 0;
+                }
+            }
+        }
+    }
+
+    private float Angle(Vector2 pos1, Vector2 pos2) {
+        Vector2 from = pos2 - pos1;
+        Vector2 to = new Vector2(1, 0);
+
+        float result = Vector2.Angle(from, to);
+        Vector3 cross = Vector3.Cross(from, to);
+
+        if (cross.z > 0) {
+            result = 360f - result;
+        }
+
+        return result;
+    }
+}
---
+++ TouchTransform/TouchTransformableObject.cs
@@ -0,0 +1,93 @@
+﻿//2018 by Daniel Pots www.vr-bits.com
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+//Allows Rotating/Scaling/Moving by Touch
+public class TouchTransformableObject : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler {
+    public bool isSelected;
+    public bool isMovable, isRotatable, isScaleable;
+    bool isTouched = false;
+    float maxScale;
+    void Start() {
+        maxScale = transform.localScale.x;
+
+        //New Object is the Selected
+        var allObjects = FindObjectsOfType<TouchTransformableObject>();
+        for (int i = 0; i < allObjects.Length; i++)
+            allObjects[i].isSelected = false;
+        isSelected = true;
+
+        //Initial Position in Front of the User
+        var screenPosition = Camera.main.WorldToViewportPoint(Camera.main.transform.position + Camera.main.transform.forward * 10);
+        Vector2 point = new Vector2(screenPosition.x, screenPosition.y);
+    }
+    void OnDrawGizmos() {
+        if (Input.touchCount == 1) {
+            var touch = Input.GetTouch(0);
+            Gizmos.DrawSphere(Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 1)), 1);
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(Vector3.ProjectOnPlane(Camera.main.ScreenToWorldPoint(touch.position), transform.up), .1f);
+        }
+    }
+    void Update() {
+        if (!isSelected)
+            return;
+        if (isMovable && isTouched) {
+            if (Input.touchCount == 1) {
+                var touch = Input.GetTouch(0);
+                if (touch.phase == TouchPhase.Moved) {
+                    //Find Parents Plane and Project Touchpoint on it
+                    var plane = new Plane(transform.parent.up, transform.parent.position);
+                    var ray = Camera.main.ScreenPointToRay(touch.position);
+                    float dist;
+                    //Raycast against Plane to position object under Touch
+                    if (plane.Raycast(ray, out dist))
+                        transform.position = ray.GetPoint(dist);
+                }
+            }
+        }
+        if (isRotatable) {
+            Quaternion desiredRotation = transform.rotation;
+            if (Mathf.Abs(TouchMovement.instance.turnAngleDelta) > 0) { // rotate
+                Vector3 rotationDeg = Vector3.zero;
+                rotationDeg.y = -TouchMovement.instance.turnAngleDelta;
+                desiredRotation *= Quaternion.Euler(rotationDeg);
+            }
+            // not so sure those will work:
+            transform.rotation = desiredRotation;
+        }
+        if (isScaleable && TouchMovement.instance.pinchDistance != 0) {
+            var value = Mathf.Clamp(transform.localScale.x + TouchMovement.instance.pinchDistanceDelta, .1f, maxScale);
+            transform.localScale = Vector3.one * value;
+        }
+    }
+    public void ResetPosition() {
+        transform.position = Vector3.zero;
+    }
+    public void ResetRotation() {
+        transform.rotation = Quaternion.identity;
+    }
+    public void ResetScale() {
+        transform.localScale = Vector3.one;
+    }
+    public void OnPointerClick(PointerEventData eventData) {
+        if (isSelected)
+            return;
+
+        //Deactivate Selection on all objects except this one
+        var allObjects = FindObjectsOfType<TouchTransformableObject>();
+        for (int i = 0; i < allObjects.Length; i++) {
+            allObjects[i].isSelected = false;
+        }
+        isSelected = true;
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        isTouched = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        isTouched = false;
+    }
+}
